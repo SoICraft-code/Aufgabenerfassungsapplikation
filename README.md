937bb737511b72b59c76647612ff298eebabe4c4
SoICraft-code <ldomke12@gmail.com>
2024-06-15 19:26:57 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Task Manager App
+
+Eine Aufgabenerfassungsapplikation zur Verwaltung von Aufgaben.
+
+## Funktionen
+
+- Aufgaben erstellen: Hinzufügen neuer Aufgaben mit Titel, Beschreibung und Fälligkeitsdatum.
+- Aufgaben anzeigen: Auflisten aller Aufgaben oder Anzeigen einer einzelnen Aufgabe.
+- Aufgaben aktualisieren: Bearbeiten bestehender Aufgaben.
+- Aufgaben löschen: Entfernen von Aufgaben aus der Liste.
+
+## Installation
+
+```bash
+pip install .
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+# Bestimmen Sie den Pfad zur README-Datei relativ zu setup.py
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="task_manager_app",  # Der Name des Pakets
+    version="0.1.0",  # Die Version des Pakets
+    author="Lukas Domke",  # Ihr Name oder der Name des Autors
+    author_email="lukas.domke@edu.fhdw.de",  # Ihre E-Mail-Adresse
+    description="Eine Aufgabenerfassungsapplikation zur Verwaltung von Aufgaben",  # Kurzbeschreibung des Pakets
+    long_description=long_description,  # Langbeschreibung des Pakets aus der README-Datei
+    long_description_content_type="text/markdown",  # Der Typ der Langbeschreibung (in diesem Fall Markdown)
+    url="https://github.com/IhrGitHubRepo",  # URL zu Ihrem GitHub-Repository oder Ihrer Projektseite
+    packages=find_packages(),  # Automatisches Finden aller Pakete und Unterpakete
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',  # Mindestanforderung für die Python-Version
+    install_requires=[
+        # Abhängigkeiten hier angeben, falls vorhanden
+        # Beispiel: 'requests', 'numpy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'task_manager=main:main',
+        ],
+    },
+)
